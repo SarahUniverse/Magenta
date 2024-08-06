81f7680225095eaa98813e505913de54b058801a
Sarah Clark <sarahireneclark@gmail.com>
2024-08-06 08:08:21 -0500

    Initial commit of project

---
+++ Magenta/MagentaTests/MagentaTests.swift
@@ -0,0 +1,16 @@
+//
+//  MagentaTests.swift
+//  MagentaTests
+//
+//  Created by Sarah Clark on 8/6/24.
+//
+
+import Testing
+
+struct MagentaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
