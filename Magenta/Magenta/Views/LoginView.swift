0c4ea27656d9be73ff8732b1a3767dcc0aadb52c
Sarah Clark <sarahireneclark@gmail.com>
2024-08-06 10:18:11 -0500

    Added SignInWithAppleButton

--- Magenta/Magenta/Views/LoginView.swift
+++ Magenta/Magenta/Views/LoginView.swift
@@ -5,11 +5,13 @@
 //  Created by Sarah Clark on 8/6/24.
 //
 
+import AuthenticationServices
 import SwiftData
 import SwiftUI
 
 struct LoginView: View {
     @Environment(\.modelContext) private var modelContext
+    @Environment(\.colorScheme) private var colorScheme
     @Query private var items: [Item]
     @State private var email: String = ""
     @State private var password: String = ""
@@ -46,7 +48,6 @@ struct LoginView: View {
             Text("Or sign in with")
                 .foregroundStyle(.secondary)
 
-            HStack (spacing: 20) {
                 Button(action: {
                     print("Google Sign-In tapped")
                 }) {
@@ -56,16 +57,19 @@ struct LoginView: View {
                         .foregroundStyle(.red)
                 }
 
-                Button(action: {
-                    print("Apple Sign-In tapped")
-                }) {
-                    Image(systemName: "apple.logo")
-                        .resizable()
-                        .frame(width: 44, height: 44)
-                        .foregroundStyle(.black)
+            SignInWithAppleButton(.continue) { request in
+                request.requestedScopes = [.fullName, .email]
+            } onCompletion: { result in
+                switch result {
+                    case .success(_):
+                        print("Authorization successful")
+                    case .failure(let error):
+                        print("Authorization failed: \(error.localizedDescription)")
                 }
             }
+            .signInWithAppleButtonStyle(colorScheme == .light ? .white : .black)
         }
+
         .padding()
     }
 
