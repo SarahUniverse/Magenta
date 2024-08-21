aff22e917ca0911eda3b3bf1b9aa246df7362686
Sarah Clark <sarahireneclark@gmail.com>
2024-08-21 14:01:56 -0500

    Initial tabview work on Overview screen

--- Magenta/Magenta/Views/Overview.swift
+++ Magenta/Magenta/Views/Overview.swift
@@ -9,9 +9,46 @@ import SwiftUI
 
 struct Overview: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .navigationTitle("Overview")
-            .navigationBarBackButtonHidden(true)
+
+        if #available(iOS 18.0, *) {
+            TabView {
+                Tab("Mood", systemImage: "face.smiling.inverse") {
+                    EmptyView()
+                }
+
+                Tab("Mood", systemImage: "face.smiling.inverse") {
+                    EmptyView()
+                }
+
+                Tab("Mood", systemImage: "face.smiling.inverse") {
+                    EmptyView()
+                }
+
+            }
+        } else {
+            // TODO: Remove when iOS 17 is no longer supported.
+            TabView {
+                Text("Home Tab Content")
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("Home")
+                    }
+
+                Text("Favorites Tab Content")
+                    .tabItem {
+                        Image(systemName: "star.fill")
+                        Text("Favorites")
+                    }
+
+                Text("Settings Tab Content")
+                    .tabItem {
+                        Image(systemName: "gear")
+                        Text("Settings")
+                    }
+            }
+        }
+            //.navigationTitle("Overview")
+            //.navigationBarBackButtonHidden(true)
     }
 }
 
