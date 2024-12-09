392bfac7182b5248b19c87724963f7461a633107
Sarah <sarahireneclark@gmail.com>
2024-12-08 23:27:48 -0600

    Added the copyright to the SignUpView

--- Magenta/Magenta/Views/MainViews/SignUpView.swift
+++ Magenta/Magenta/Views/MainViews/SignUpView.swift
@@ -69,7 +69,11 @@ struct SignUpView: View {
                 Text(signUpViewModel.errorMessage)
                     .foregroundColor(.red)
                     .padding()
+                Spacer()
 
+                Text("© \(String(Calendar.current.component(.year, from: Date()))) SarahUniverse")
+                    .font(.caption)
+                    .foregroundColor(.white.opacity(0.8))
                 Spacer()
             }
             .onAppear {
