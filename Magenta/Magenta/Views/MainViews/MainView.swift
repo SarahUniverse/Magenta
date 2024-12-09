c9e60fc16d34dee1340ccf288371088b54afd68f
Sarah <sarahireneclark@gmail.com>
2024-12-09 01:25:43 -0600

    Removed iOS 17 specific tabview code

--- Magenta/Magenta/Views/MainViews/MainView.swift
+++ Magenta/Magenta/Views/MainViews/MainView.swift
@@ -11,53 +11,15 @@ struct MainView: View {
     let user: UserModel
 
     var body: some View {
-
-        if #available(iOS 18.0, *) {
-            TabView {
-                Tab("Summary", systemImage: "heart.fill") { SummaryView() }
-                Tab("Mood", systemImage: "face.smiling.inverse") { MoodView() }
-                Tab("Meditate", systemImage: "figure.mind.and.body") { MeditateView() }
-                Tab("Exercise", systemImage: "figure.run") { ExerciseView() }
-                Tab("More", systemImage: "line.3.horizontal") { MoreView() }
-            }
-            .navigationBarBackButtonHidden(true)
-        } else {
-            // TODO: Remove when iOS 17 is no longer supported.
-            TabView {
-                SummaryView()
-                .tabItem {
-                    Image(systemName: "heart.fill")
-                    Text("Summary")
-                }
-
-                MoodView()
-                .tabItem {
-                        Image(systemName: "face.smiling.inverse")
-                        Text("Mood")
-                    }
-
-                MeditateView()
-                    .tabItem {
-                        Image(systemName: "figure.mind.and.body")
-                        Text("Meditate")
-                    }
-
-                ExerciseView()
-                    .tabItem {
-                        Image(systemName: "figure.run")
-                        Text("Exercise")
-                    }
-                MoreView()
-                    .tabItem {
-                        Image(systemName: "line.3.horizontal")
-                        Text("More")
-                    }
-            }
-            .tabViewStyle(DefaultTabViewStyle())
-            .navigationBarBackButtonHidden(true)
+        TabView {
+            Tab("Summary", systemImage: "heart.fill") { SummaryView() }
+            Tab("Mood", systemImage: "face.smiling.inverse") { MoodView() }
+            Tab("Meditate", systemImage: "figure.mind.and.body") { MeditateView() }
+            Tab("Exercise", systemImage: "figure.run") { ExerciseView() }
+            Tab("More", systemImage: "line.3.horizontal") { MoreView() }
         }
+        .navigationBarBackButtonHidden(true)
     }
-
 }
 
 #Preview {
