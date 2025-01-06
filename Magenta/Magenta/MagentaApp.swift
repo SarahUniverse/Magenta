2ab5877955909895b3bd4f8aa71f073355a4d7db
Sarah <sarahireneclark@gmail.com>
2025-01-06 12:12:58 -0600

    Added CoreDataStack

---
+++ Magenta/Magenta/MagentaApp.swift
@@ -0,0 +1,28 @@
+//
+//  MagentaApp.swift
+//  Magenta
+//
+//  Created by Sarah Clark on 8/6/24.
+//
+
+import CoreData
+import GoogleSignIn
+import SwiftUI
+
+@main
+struct MagentaApp: App {
+// Create an observable instance of the Core Data stack.
+    @StateObject private var coreDataStack = CoreDataStack.shared
+
+    var body: some Scene {
+        let users: [UserModel] = []
+        WindowGroup {
+            InitialView()
+                .onOpenURL { url in
+                    GIDSignIn.sharedInstance.handle(url)
+                }
+                // Inject the persistent container's managed object context into the environment
+                .environment(\.managedObjectContext, coreDataStack.persistentContainer.viewContext)
+        }
+    }
+}
---
+++ Magenta/Magenta/Models/CoreDataStack.swift
@@ -0,0 +1,30 @@
+//
+//  CoreDataStack.swift
+//  Magenta
+//
+//  Created by Sarah Clark on 1/6/25.
+//
+
+import Combine
+import CoreData
+
+class CoreDataStack: ObservableObject {
+    static let shared = CoreDataStack()
+
+    // Create a persistent container as a lazy variable to defer instantiation until its first use.
+    lazy var persistentContainer: NSPersistentContainer = {
+
+        // Pass the data model filename to the container’s initializer.
+        let container = NSPersistentContainer(name: "DataModel")
+
+        // Load any persistent stores, which creates a store if none exists.
+        container.loadPersistentStores { _, error in
+            if let error {
+                fatalError("Failed to load persistent stores: \(error.localizedDescription)")
+            }
+        }
+        return container
+    }()
+
+    private init() { }
+}
