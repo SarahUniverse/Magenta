1995a17f1e173bab3c9e66a8a0651a2508a57bd4
Sarah <sarahireneclark@gmail.com>
2025-01-06 13:46:42 -0600

    Added UserEntity

---
+++ Magenta/UserEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  UserEntity+CoreDataClass.swift
+//  Magenta
+//
+//  Created by Sarah Clark on 1/6/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(UserEntity)
+public class UserEntity: NSManagedObject {
+
+}
---
+++ Magenta/UserEntity+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  UserEntity+CoreDataProperties.swift
+//  Magenta
+//
+//  Created by Sarah Clark on 1/6/25.
+//
+//
+
+import CoreData
+import Foundation
+
+extension UserEntity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<UserEntity> {
+        return NSFetchRequest<UserEntity>(entityName: "UserEntity")
+    }
+
+    @NSManaged public var id: UUID?
+    @NSManaged public var username: String?
+
+}
+
+extension UserEntity : Identifiable {
+
+}
