79a4ec8ce6f69c2c637f2a5f526d223fcc952a45
Sarah <sarahireneclark@gmail.com>
2025-01-19 20:40:26 -0600

    Added background gradient for MeditateView

--- Magenta/Magenta/Meditation/Views/MeditateView.swift
+++ Magenta/Magenta/Meditation/Views/MeditateView.swift
@@ -10,14 +10,25 @@ import SwiftUI
 struct MeditateView: View {
     @StateObject private var viewModel = MeditateViewModel()
 
+    let backgroundGradient = LinearGradient(
+        stops: [
+            Gradient.Stop(color: .cyan, location: 0),
+            Gradient.Stop(color: .darkBlue.opacity(0.7), location: 0.1),
+            Gradient.Stop(color: .darkBlue.opacity(0.7), location: 0.2),
+            Gradient.Stop(color: .clear, location: 0.4)
+        ],
+        startPoint: .top,
+        endPoint: .bottom
+    )
+
     var body: some View {
         NavigationStack {
             List(viewModel.meditationSessions, id: \.self) { session in
                 Text(session)
             }
             .navigationTitle("Meditate")
-            .toolbarBackground(.darkBlue, for: .navigationBar)
-            .toolbarBackground(.visible, for: .navigationBar)
+            .background(backgroundGradient)
+            .scrollContentBackground(.hidden)
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Image(systemName: "person.circle")
