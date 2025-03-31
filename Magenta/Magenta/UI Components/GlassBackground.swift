afb70ee6a0a88ba19d22a7d2ad48dab4a57d466e
Sarah <sarahireneclark@gmail.com>
2025-03-31 10:19:30 -0500

    Removed unneeded SwiftData related class and added a new file

---
+++ Magenta/Magenta/UI Components/GlassBackground.swift
@@ -0,0 +1,18 @@
+//
+//  GlassBackground.swift
+//  Magenta
+//
+//  Created by Sarah Clark on 3/31/25.
+//
+
+import SwiftUI
+
+struct GlassBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GlassBackground()
+}
