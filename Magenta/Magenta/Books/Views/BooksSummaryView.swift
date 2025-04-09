a07ab563073d7de4a1a147736e234136cf415ac1
Sarah Clark <sarahireneclark@gmail.com>
2025-04-09 09:25:49 -0500

    Added NavigationLink to BooksSummaryView

--- Magenta/Magenta/Books/Views/BooksSummaryView.swift
+++ Magenta/Magenta/Books/Views/BooksSummaryView.swift
@@ -17,31 +17,33 @@ struct BooksSummaryView: View {
         _booksViewModel = State(wrappedValue: BooksViewModel(viewContext: viewContext))
     }
 
-    // MARK: Body
+    // MARK: - Body
     var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
-            Text("BOOKS")
-                .font(.caption)
-                .fontWeight(.bold)
-                .foregroundStyle(.gray)
+        NavigationLink(destination: BooksView(viewContext: viewContext)) {
+            VStack(alignment: .leading, spacing: 10) {
+                Text("BOOKS")
+                    .font(.caption)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.gray)
 
-            HStack(alignment: .top, spacing: 10) {
-                Image(systemName: "books.vertical")
-                    .foregroundStyle(.brown)
-                    .font(.largeTitle)
+                HStack(alignment: .top, spacing: 10) {
+                    Image(systemName: "books.vertical")
+                        .foregroundStyle(.brown)
+                        .font(.largeTitle)
 
-                VStack(alignment: .leading, spacing: 5) {
-                    Text("Make sure the Health features on your iPhone and Apple Watch are set up the way you want them.")
-                        .font(.subheadline)
-                        .foregroundStyle(.gray)
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Make sure the Health features on your iPhone and Apple Watch are set up the way you want them.")
+                            .font(.subheadline)
+                            .foregroundStyle(.gray)
 
-                    Button("Review") { }
-                        .foregroundStyle(.blue)
+                        Button("Review") { }
+                            .foregroundStyle(.blue)
+                    }
                 }
+                .padding()
+                .background(GlassBackground())
+                .cornerRadius(10)
             }
-            .padding()
-            .background(GlassBackground())
-            .cornerRadius(10)
         }
     }
 
