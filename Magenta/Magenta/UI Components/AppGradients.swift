813e676aecd2b0ff4a106773c5bfd8222a5c9f08
Sarah Clark <sarahireneclark@gmail.com>
2025-04-10 14:42:16 -0500

    Added universal file for AppGradients to use throughout the app

---
+++ Magenta/Magenta/UI Components/AppGradients.swift
@@ -0,0 +1,17 @@
+//
+//  AppStyles.swift
+//  Magenta
+//
+//  Created by Sarah Clark on 4/10/25.
+//
+
+import SwiftUI
+
+struct AppGradients {
+    static let backgroundGradient = LinearGradient(
+        gradient: Gradient(colors: [Color.blue.opacity(0.6), Color.purple.opacity(0.4)]),
+        startPoint: .topLeading,
+        endPoint: .bottomTrailing
+    )
+
+}
